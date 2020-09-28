de59c0ebc8d3363c1528f1159f0d335750dc63b4
HariniMurali06 <harinimurali.jv@gmail.com>
2020-09-29 00:06:56 +0530

    Changed Login UI

---
+++ lib/Screens/Login/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:Demen_Buster/Screens/Login/components/body.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
---
+++ lib/Screens/components/body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override 
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size; // This size provides us the total height and width of our screen
+    return Container();
+  }
+}
