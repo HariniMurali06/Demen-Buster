f53cf15f69c22e8f4b4ba5d12ab5c103147bb224
santhoshunt <santhoshkumar2012002@gmail.com>
2020-10-11 19:18:26 +0530

    'del'

--- lib/Screens/components/body.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Body extends StatelessWidget {
-  @override 
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size; // This size provides us the total height and width of our screen
-    return Container();
-  }
-}
