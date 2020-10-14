6061ee0f739d73b8fc16e1bebc3756a50f131f7b
HariniMurali06 <54319804+HariniMurali06@users.noreply.github.com>
2020-10-14 16:10:14 +0530

    Integrated Game files

--- lib/screens/diagnose/diagnose.dart
+++ lib/screens/diagnose/diagnose.dart
@@ -1,15 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:Demen_Buster/screens/diagnose/game_choose.dart';
+import 'package:Demen_Buster/screens/diagnose/games/simon.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:Demen_Buster/screens/home/home.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class Diagnose extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Diagnose Game choose Screen"),
-        backgroundColor: Colors.green[600],
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(
+            MediaQuery.of(context).size.height * 0.045,
+            MediaQuery.of(context).size.height * 0.1,
+            MediaQuery.of(context).size.height * 0.045,
+            MediaQuery.of(context).size.height * 0.02),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Take a pick!",
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline
+                  .copyWith(fontSize: 39, color: Colors.blueGrey[600]),
+            ),
+            Text(
+              "Let's play some games to put your memory to test. Go ahead and play the games!",
+              style: Theme.of(context)
+                  .textTheme
+                  .title
+                  .copyWith(color: Colors.black54),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.044,
+            ),
+            Column(
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => GameChoose(),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(27.0),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      title: Text(
+                        "Play mini games",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .body1
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SimonGame(),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    color: Colors.lightBlueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(27.0),
+                    ),
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      title: Text(
+                        "Take the full test",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .body1
+                            .copyWith(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/gamer.gif'), fit: BoxFit.cover),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.037,
+            ),
+            GestureDetector(
+              onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Home(),
+                      ),
+                    );
+                  },
+              child: 
+                   Icon(
+                      Icons.logout,
+                      color: Colors.black,
+                    ),
+            ),
+          ],
+        ),
       ),
-      body: Text("Take your pick screen will be here"),
     );
   }
+  
 }
