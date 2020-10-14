6b09d2e609152eff245b8c35b09eda2bae27c05b
santhoshunt <santhoshkumar2012002@gmail.com>
2020-10-15 01:31:18 +0530

    Map

--- lib/screens/carecentres/details.dart
+++ lib/screens/carecentres/details.dart
@@ -9,37 +9,69 @@ class Details extends StatelessWidget {
     data = ModalRoute.of(context).settings.arguments;
     print(data.toString());
     return Scaffold(
-      appBar: AppBar(
-        title: Text(data['name']),
-      ),
-      body: Container(
-        child: Card(
-          child: Column(
-            children: [
-              Text(
-                data['name'],
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
+      appBar: AppBar(title: Text(data['type']), backgroundColor: Colors.green),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 50,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        data['name'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        data['address'],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        FlatButton.icon(
+                          icon: Icon(Icons.phone),
+                          label: Text("Call"),
+                          onPressed: _launchURL,
+                        ),
+                        FlatButton.icon(
+                          icon: Icon(Icons.location_on),
+                          label: Text("Google Maps"),
+                          onPressed: _launchMAP,
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                data['address'],
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                ),
-              ),
-              FlatButton.icon(
-                icon: Icon(Icons.phone),
-                label: Text("Call"),
-                onPressed: _launchURL,
-              ),
-            ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  _launchMAP() async {
+    if (await canLaunch(
+        "https://www.google.co.in/maps/place/${data['address']}")) {
+      await launch("https://www.google.co.in/maps/place/${data['address']}");
+    } else {
+      throw 'Could not launch https://www.google.co.in/maps/place/${data['address']}';
+    }
   }
 
   _launchURL() async {
